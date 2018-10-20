54fb4b4cf6940ff4cb39db3f403a6c18396096e8
chanchaltyagi <chanchal.tyagi@gmail.com>
2018-10-20 09:13:33 +0530

    Config Client changes

--- src/main/java/io/chanchal/cloud/configclient/ConfigClientAppConfiguration.java
+++ src/main/java/io/chanchal/cloud/configclient/ConfigClientAppConfiguration.java
@@ -9,6 +9,15 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "some")
 public class ConfigClientAppConfiguration {
 
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
 }
 
 
--- src/main/java/io/chanchal/cloud/configclient/ConfigClientApplication.java
+++ src/main/java/io/chanchal/cloud/configclient/ConfigClientApplication.java
@@ -1,12 +1,35 @@
 package io.chanchal.cloud.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ConfigClientApplication {
+
+	@Autowired
+	private  ConfigClientAppConfiguration properties;
+
+	@Value("${some.other.property}")
+	String someOtherProperty ;
+
+
+	@RequestMapping("/")
+	public String printProperties()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append(properties.getProperty());
+		sb.append("||");
+		sb.append(someOtherProperty);
+
+		return sb.toString();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
